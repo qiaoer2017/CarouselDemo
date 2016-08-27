1a2d4e6a8d44f9e194829404782d0980f049c2f8
qiaoer <qiaoer2017@163.com>
2016-08-27 17:21:33 +0800

    实现无限滚动以及改变指示器状态

--- script.js
+++ script.js
@@ -8,15 +8,48 @@ window.onload = function () {
     var prev = document.getElementById("prev");
     var next = document.getElementById("next");
 
+    var currentIndex = 1;
+
     next.onclick = function () {
+        if (currentIndex >= 5) {
+            currentIndex = 1;
+        } else {
+            currentIndex += 1;
+        }
         switchingCurrentItem(-600);
+        changeIndicator();
     };
     prev.onclick = function () {
+        if (currentIndex <= 1) {
+            currentIndex = 5;
+        } else {
+            currentIndex -= 1;
+        }
         switchingCurrentItem(600);
+        changeIndicator();
     };
 
-};
+    function switchingCurrentItem(offset) {
+        var newLeft = parseInt(list.style.left) + offset;
+        list.style.left = newLeft + 'px';
 
-function switchingCurrentItem(offset) {
-    list.style.left = (parseInt(list.style.left) + offset) + 'px';
-}
+        if (newLeft > -600) {
+            list.style.left = -3000 + 'px';
+        }
+        if (newLeft < -3000) {
+            list.style.left = -600 + 'px';
+        }
+    }
+
+    function changeIndicator() {
+        for (var i = 0; i < buttons.length; i++) {
+            if (buttons[i].className == 'on') {
+                // debugger;
+                buttons[i].className = '';
+                break;
+            }
+        }
+        buttons[currentIndex - 1].className = 'on';
+    }
+
+};
